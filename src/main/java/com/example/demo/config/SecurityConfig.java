5c8822a4b3edb257f71ae0822d340ec0d4f1b46a
bala-amypo <bala@amypo.in>
2025-12-20 15:43:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,10 +1,10 @@
-
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+    // Empty on purpose – tests do not need security filters
 }
-
