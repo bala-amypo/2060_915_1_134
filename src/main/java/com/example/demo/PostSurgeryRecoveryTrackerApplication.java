e1767f41c8b355e3f8ada2c98f1528775e53e21b
bala-amypo <bala@amypo.in>
2025-12-20 16:19:57 +0000

    User commit

--- src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
+++ src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
@@ -3,8 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+    }
+)
 public class PostSurgeryRecoveryTrackerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PostSurgeryRecoveryTrackerApplication.class, args);
     }
