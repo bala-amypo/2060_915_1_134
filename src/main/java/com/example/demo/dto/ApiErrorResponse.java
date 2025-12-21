a32dc1905b84d5ff40045ebeb2b9ba961b6b462c
bala-amypo <bala@amypo.in>
2025-12-21 18:05:12 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiErrorResponse.java
+++ src/main/java/com/example/demo/dto/ApiErrorResponse.java
@@ -1,20 +1,10 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiErrorResponse {
-
     private String message;
-
-    public ApiErrorResponse() {}
-
-    public ApiErrorResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
