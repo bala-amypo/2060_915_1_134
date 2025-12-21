baeb6a4a5c4887f2e59edde58be4d5370d38f085
bala-amypo <bala@amypo.in>
2025-12-21 19:10:11 +0000

    User commit

--- src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
+++ src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PostSurgeryRecoveryTrackerApplication {
 
     public static void main(String[] args) {
-        System.out.println("=================================");
-        System.out.println("APPLICATION STARTED SUCCESSFULLY");
-        System.out.println("SPRING BOOT ENVIRONMENT IS WORKING");
-        System.out.println("=================================");
+        SpringApplication.run(PostSurgeryRecoveryTrackerApplication.class, args);
     }
 }
