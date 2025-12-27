ca92f12894f7f229c9ce61bb40d0a6e058347bdb
bala-amypo <bala@amypo.in>
2025-12-27 15:56:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,15 +1,16 @@
 package com.example.demo.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
-    
-    @NotBlank
     @Email
+    @NotBlank
     private String email;
-    
+
     @NotBlank
     @Size(min = 6)
     private String password;
